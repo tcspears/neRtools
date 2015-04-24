e818baebbb24b1cfeea772e28cdeb10e95637a4e
Taylor Spears <tcs@tcspears.net>
2015-04-24 17:02:57 +0100

    Added @return documentation, changed var names

--- R/nlp_engine.R
+++ R/nlp_engine.R
@@ -3,10 +3,11 @@
 #' Builds an Annotated Plaintext Document from a character vector input.
 #' @param input A character vector of text to be analysed.
 #' @param model.list A model list, produced using load_nlp_models.
+#' @return An annotated plaintext document for input.
 
 nlp_engine <- function(input,model.list){
   s <- as.String(input)
-  s_annotations <- annotate(s,model.list)
-  s_doc <- AnnotatedPlainTextDocument(s, s_annotations)
-  return(s_doc)
+  ann <- annotate(s,model.list)
+  doc <- AnnotatedPlainTextDocument(s, ann)
+  return(doc)
 }
